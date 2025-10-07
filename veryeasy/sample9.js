906ebd955cd57dec9e22b1d11edb945fdaae8411
ks24pon <pom123re0624@gmail.com>
2025-10-07 17:21:33 +0900

    https://recursionist.io/dashboard/problems/384

---
+++ veryeasy/sample9.js
@@ -0,0 +1,14 @@
+let myPower = 300;
+let mySpeed = 200;
+let myTechnique = 50;
+let myDefense = 500;
+
+myPower += 10;
+mySpeed -= 5;
+myTechnique *= 7;
+myDefense /= 2;
+
+console.log("myPower : " + myPower);
+console.log("mySpeed : " + mySpeed);
+console.log("myTechnique : " + myTechnique);
+console.log("myDefense : " + myDefense);
