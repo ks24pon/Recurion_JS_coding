927acf41854d408a0a5abf07754d5cccf3912d07
ks24pon <pom123re0624@gmail.com>
2025-10-07 13:51:41 +0900

    https://recursionist.io/dashboard/problems/391

---
+++ easy/sample1.js
@@ -0,0 +1,4 @@
+console.log("Computer".length);
+console.log("recursion@info.com".length);
+console.log("Computer"["Computer".length-1]);
+console.log("recursion@info.com"["recursion@info.com".length-1]);
