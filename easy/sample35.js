7fc8ff9e0717f501fc232118e9228a226904e329
ks24pon <pom123re0624@gmail.com>
2025-10-21 10:22:58 +0900

    https://recursionist.io/dashboard/course/2/lesson/197

---
+++ easy/sample35.js
@@ -0,0 +1,10 @@
+function isPrime(number){
+  if(number == 1) return false;
+  if(number == 2) return true;
+  for(let i = 2; i < number; i++) {
+      if(number % i == 0) {
+          return false;
+      }
+  }
+  return true;
+}
